73764920c43d80fae131b4f74324d6952f404914
Nir Meron <96244260+nickmeron@users.noreply.github.com>
2022-11-02 10:34:15 +0200

    Add files via upload

---
+++ Assignment - JS – if, console, let/script.js
@@ -0,0 +1,40 @@
+let response = "";
+let toppings,isTopping = "";
+
+function order(clicked_id)
+  {
+      response=clicked_id;
+        completeOrder();
+  }
+
+  function completeOrder() {
+    switch(response) {
+        case "bur":
+           isTopping = prompt("Great Choice, Would you like add anything to your burger? (Yes/No)");
+          if (isTopping=="yes") {
+            toppings = prompt("Please type down you desired toppings, (Lettus,Tomtats,Onion,Extra Petty)");
+            alert("Perfect, we have got your order: Burger with "+ toppings);
+          }
+          else if (isTopping=="no"){
+            alert("Ok then, Burger it is!");
+          }
+          else {
+            alert("Please type (Yes/No)");
+          }
+          break;
+        case "piz":
+             isTopping = prompt("Great Choice, Would you like add anything to your pizza? (Yes/No)");
+            if (isTopping=="yes") {
+              toppings = prompt("Please type down you desired toppings, (Olives,Tomtats,Onion,Extra Cheese)");
+              alert("Perfect, we have got your order: Pizza with "+ toppings);
+            }
+            else if (isTopping=="no"){
+            alert("Ok then, Pizza it is!");
+          }
+          else {
+            alert("Please type (Yes/No)");
+          }
+          break;
+      }
+  }
+  
